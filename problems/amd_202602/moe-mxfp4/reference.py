cb561a1e78183f931d8eb8d38c3f99cc6a3c6b12
zhutaoyu <zhutaoyu97@gmail.com>
2026-02-12 03:49:25 +0000

    change time counter

--- problems/amd_202602/moe-mxfp4/reference.py
+++ problems/amd_202602/moe-mxfp4/reference.py
@@ -320,4 +320,4 @@ def ref_kernel(data: input_t) -> output_t:
 
 
 
-check_implementation = make_match_reference(ref_kernel, rtol=1e-2, atol=1e-2)
+check_implementation = make_match_reference(ref_kernel, rtol=5e-2, atol=5e-2)
